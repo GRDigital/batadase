3da226e6445f970325e309bdbd031a34e816fcfd
Gremious Grenald <gremious@gremy.co.uk>
2023-11-07 18:10:19 +0000

    set version fn

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,10 @@ pub fn version() -> semver::Version {
 	semver::Version::parse(VERSION.trim()).expect("Can't parse version")
 }
 
+pub fn set_version(version: semver::Version) -> std::io::Result<()> {
+	std::fs::write("db/version", version.to_string())
+}
+
 pub fn verify(expected: &semver::Version) {
 	let version = version();
 	assert!(version == *expected, "DB version error: expected {expected}, but DB was found at {version}.");
