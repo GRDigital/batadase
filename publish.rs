a7590467d63b65ab952c42017a966286eec3e131
Awpteamoose <awpteamoose@gmail.com>
2024-09-20 16:19:49 +0100

    fix dirs

--- publish.rs
+++ publish.rs
@@ -3,9 +3,9 @@ use std::time::Duration;
 use std::process::Command;
 
 fn main() {
-	assert!(Command::new("cargo").arg("publish").current_dir("batadase-index").status().unwrap().success());
+	assert!(Command::new("cargo").arg("publish").current_dir("index").status().unwrap().success());
 	sleep(Duration::from_secs(60));
-	assert!(Command::new("cargo").arg("publish").current_dir("batadase-macros").status().unwrap().success());
+	assert!(Command::new("cargo").arg("publish").current_dir("macros").status().unwrap().success());
 	sleep(Duration::from_secs(60));
-	assert!(Command::new("cargo").arg("publish").current_dir("batadase").status().unwrap().success());
+	assert!(Command::new("cargo").arg("publish").status().unwrap().success());
 }
